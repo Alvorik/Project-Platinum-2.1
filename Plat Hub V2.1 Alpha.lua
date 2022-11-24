64b1fefc6cefe90a324afb60a3802c25384d7444
Alvorik <115509470+Alvorik@users.noreply.github.com>
2022-11-23 22:13:28 -0600

    Update Plat Hub V2.1 Alpha.lua

--- Plat Hub V2.1 Alpha.lua
+++ Plat Hub V2.1 Alpha.lua
@@ -21,7 +21,7 @@ MainSection:NewButton("Admin Commands #2", "i have no idea what the name of the 
     loadstring(game:HttpGet('\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\79\109\110\105\112\111\116\101\110\99\101\68\101\118\101\108\111\112\101\114\47\78\117\109\98\101\114\47\109\97\105\110\47\49\46\108\117\97'))()
 end)
 
-MainSection:NewLabel("Made By Alvorik/エド2kk#0001")
+MainSection:NewLabel("Made By Alvorik/エド2kk#5160")
 
 MainSection:NewButton("Copy Discord Link", "Copy Alvorik's World's Discord Server", function()
 	setclipboard("https://discord.gg/ykHhHkK7YF")
@@ -66,14 +66,14 @@ if game.PlaceId == 537413528 then
 	local BLBSection = BLB:NewSection("Auto-Make")
 
     BLBSection:NewButton("Auto-Build", "put the file in ur workspace", function()
-        loadstring(game:HttpGet(('https://pastebin.com/raw/T6Tqqqvj'), true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Alvorik/BBA-item-import/main/build_a_boat_bronide.lua"))()
     end)
 
 	BLBSection:NewButton("Auto-Image", "put the file in ur workspace", function()
 		loadstring(game:HttpGet(('https://pastebin.com/raw/T6Tqqqvj'), true))()
 	end)
     
-    BLBSection:NewLabel("Autofarm")
+    BLBSection:NewButton("")
 
 	BLBSection:NewButton("Vynixius", "sub to him :)", function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/Script.lua"))()
